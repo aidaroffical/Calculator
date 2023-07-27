56ebdcab6f0abd82d1ba895d30aa6ee6a4b4b278
Aidar4ik <aidaroffical@mail.ru>
2023-07-27 18:43:30 +0600

    Initial commit

---
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -0,0 +1,97 @@
+package com.example.calculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.text.DecimalFormat;
+
+public class MainActivity extends AppCompatActivity {
+    private TextView textView;
+    private double firstVar, secondVar;
+    private boolean isOperationClick;
+    private String operation;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        textView = findViewById(R.id.text_view);
+    }
+
+    public void onNumberClick(View view) {
+        String number = ((Button) view).getText().toString();
+        setTv_Result(number);
+    }
+
+    public void setTv_Result(String number) {
+        if (textView.getText().toString().equals("0") || isOperationClick) {
+            textView.setText(number);
+        } else {
+            textView.append(number);
+        }
+        isOperationClick = false;
+    }
+
+    public void onOperationClick(View view) {
+        int viewId = view.getId();
+        if (viewId == R.id.btn_clear) {
+            textView.setText("0");
+            firstVar = 0.0;
+            secondVar = 0.0;
+        } else if (viewId == R.id.btn_dot) {
+            if (!textView.getText().toString().contains(".")) {
+                textView.append(".");
+            }
+        } else if (viewId == R.id.btn_percent) {
+            firstVar = Double.parseDouble(textView.getText().toString());
+            Double result = firstVar / 100;
+            textView.setText(new DecimalFormat("##.#######").format(result));
+        } else if (viewId == R.id.btn_multiplay) {
+            firstVar = Double.parseDouble(textView.getText().toString());
+            firstVar *= -1;
+            textView.setText(new DecimalFormat("##.#######").format(firstVar));
+        } else if (viewId == R.id.btn_plus) {
+            performOperation("+");
+        } else if (viewId == R.id.btn_minus) {
+            performOperation("-");
+        } else if (viewId == R.id.btn_multiplication) {
+            performOperation("X");
+        } else if (viewId == R.id.btn_slash) {
+            performOperation("/");
+        } else if (viewId == R.id.btn_equals) {
+            calculateResult();
+        }
+    }
+
+    private void performOperation(String op) {
+        firstVar = Double.parseDouble(textView.getText().toString());
+        operation = op;
+        isOperationClick = true;
+    }
+
+    private void calculateResult() {
+        if (!isOperationClick) {
+            secondVar = Double.parseDouble(textView.getText().toString());
+            double result = 0;
+            switch (operation) {
+                case "+":
+                    result = firstVar + secondVar;
+                    break;
+                case "-":
+                    result = firstVar - secondVar;
+                    break;
+                case "X":
+                    result = firstVar * secondVar;
+                    break;
+                case "/":
+                    result = firstVar / secondVar;
+                    break;
+            }
+            textView.setText(new DecimalFormat("##.#######").format(result));
+        }
+    }
+}
